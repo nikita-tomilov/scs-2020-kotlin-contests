8c6e824ea1593b7ccc63a84b4d160813876c5465
Nikita Tomilov <programmer174@icloud.com>
2020-08-14 13:20:26 +0500

    all: move kotlin sources under maven

--- build-tasks.sh
+++ build-tasks.sh
@@ -4,8 +4,8 @@ mkdir -p ./compiled-tasks
 
 echo "%%%%%%%% CONTEST C1 %%%%%%%%"
 echo ""
-/usr/bin/env python3 build-tasks-for-contest.py c1 ./statements ./compiled-tasks ./src ./test/resources
+/usr/bin/env python3 build-tasks-for-contest.py c1 ./statements ./compiled-tasks ./src/main/kotlin ./src/test/resources
 
 echo "%%%%%%%% CONTEST C2 %%%%%%%%"
 echo ""
-/usr/bin/env python3 build-tasks-for-contest.py c2 ./statements ./compiled-tasks ./src ./test/resources
+/usr/bin/env python3 build-tasks-for-contest.py c2 ./statements ./compiled-tasks ./src/main/kotlin ./src/test/resources
